20797d8f1438a7e904116da5d97cc48efe69c866
Raul Garcia <raulgarcia01@gmail.com>
2023-03-15 10:35:13 -0700

    Fix publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-	publicPath: process.env.NODE_ENV === 'production' ? '/babylon-vue' : '/',
+	publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
 	transpileDependencies: true,
 	outputDir: "./static",
 });
